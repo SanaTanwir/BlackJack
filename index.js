af48f2b2e02a76583ba16e60db8e6c1dc4ff586f
SanaTanwir <156840355+SanaTanwir@users.noreply.github.com>
2024-10-02 12:13:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 
-//  methods are functions () attach to an object
-//  creating object after assingment operator {} inside a : instead of = and seprated by ,
+
 
 let player = {
     name :"Sana",
@@ -21,18 +20,16 @@ let message = ""
 
 
 
-// to log message on the screen
+
 let messageEl = document.getElementById ("message-el")
 let cardsEl = document.getElementById ("cards-el")
 let sumEl = document.getElementById("sum-el")
 let playerEl = document.getElementById ("player-el")
 
-// calling from object name and the subname
+
 playerEl.textContent = player.name + " : £" + player.chips
 
-//  another way of doing get element by id is querySelector where selector are css selector such as bidy but you specify id #
-//  you can also change the to class in html but use . instead of #
-// e.g let sumEl = document.querySelector ("#sum-el")
+
     
 
 
@@ -40,8 +37,7 @@ playerEl.textContent = player.name + " : £" + player.chips
 
 function randomCard() {
     let randomNumber = Math.floor ( Math.random() * 13) + 1
-    //  black jack range 1-13, 11-13 has value 10, Ace/1 has value 11
-
+   
     if ( randomNumber > 10 ) {
         return 10
     }
@@ -75,7 +71,7 @@ cards = [firstCard, secondCard]
 function playGame() {
       
 
-        //  to put  on the screen
+       
          cardsEl.textContent = "Cards: " 
 //  for loop for adding cards
 
@@ -100,20 +96,19 @@ else  {
     isAlive = false
    
 }
-    // console.log (message) for the message on console,  to log on the website use .textContent
+   
 
     messageEl.textContent =  message
     }
 
    function newCard(){
    
-    // console.log ("Drawing new card from the deck!")
-    // to stop the new card from generating after losing or winning 
+   
    
     if ( isAlive == true && hasBlackJack == false) {
     let card = randomCard ()
     sum += card
-    // to add the card in an array, pop to remove
+     
     cards.push (card)
     playGame()
     }
